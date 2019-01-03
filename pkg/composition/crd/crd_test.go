04533c1bb9af8f784bafbdf2f6ead939eeee570e
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2019-01-03 21:27:17 +1100

    Use typed schema conversion rather than going to/from JSON

--- pkg/composition/crd/crd_test.go
+++ pkg/composition/crd/crd_test.go
@@ -1,37 +1,17 @@
 package crd
 
 import (
-	"encoding/json"
 	"testing"
 
+	"github.com/atlassian/voyager/pkg/api/schema/schematest"
 	"github.com/atlassian/voyager/pkg/util/testutil"
 	"github.com/ghodss/yaml"
-	"github.com/go-openapi/errors"
-	"github.com/go-openapi/spec"
-	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/validate"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func loadSchemaForCRD(t *testing.T) *spec.Schema {
-	crd := ServiceDescriptorCrd()
-
-	// ugly way of loading a schema is to marshal to JSON to get the schema
-	// and then back into a schema object
-	b, err := json.Marshal(crd.Spec.Validation.OpenAPIV3Schema)
-	require.NoError(t, err)
-
-	var schemaProps spec.SchemaProps
-	err = json.Unmarshal(b, &schemaProps)
-	require.NoError(t, err)
-
-	return &spec.Schema{
-		SchemaProps: schemaProps,
-	}
-}
-
-func runCRDTestCase(t *testing.T, schema *spec.Schema, filename string, errorMsg string) {
+func runCRDTestCase(t *testing.T, schemaValidator *validate.SchemaValidator, filename string, errorMsg string) {
 	srcFile, err := testutil.LoadFileFromTestData(filename)
 	require.NoError(t, err)
 
@@ -39,20 +19,15 @@ func runCRDTestCase(t *testing.T, schema *spec.Schema, filename string, errorMsg
 	err = yaml.Unmarshal(srcFile, &testData)
 	require.NoError(t, err)
 
-	err = validate.AgainstSchema(schema, testData, strfmt.Default)
+	result := schemaValidator.Validate(testData)
 
 	if errorMsg == "" {
-		assert.NoError(t, err)
+		assert.NoError(t, result.AsError())
 		return
 	}
 
-	require.Error(t, err)
-
-	// validation errors are returned as a composite error
-	ce, ok := err.(*errors.CompositeError)
-	require.True(t, ok)
-
-	assert.EqualError(t, ce.Errors[0], errorMsg)
+	require.Error(t, result.AsError())
+	assert.EqualError(t, result.Errors[0], errorMsg)
 }
 
 func TestServiceDescriptorSchema(t *testing.T) {
@@ -75,12 +50,12 @@ func TestServiceDescriptorSchema(t *testing.T) {
 		},
 	}
 
-	schema := loadSchemaForCRD(t)
+	schemaValidator := schematest.SchemaValidatorForCRD(t, ServiceDescriptorCrd())
 
 	for _, tc := range cases {
 		filename := tc.name + ".yml" // files are all yaml files
 		t.Run(filename, func(t *testing.T) {
-			runCRDTestCase(t, schema, filename, tc.errorMsg)
+			runCRDTestCase(t, schemaValidator, filename, tc.errorMsg)
 		})
 	}
 
