954134d6b377b63048c1852f93fa9665362e9d4e
Adam Czepil <aczepil@atlassian.com>
2019-01-09 17:04:37 +1100

    Added shape for KubeIngress'es endpoint annotation to be consumed by internaldns

--- pkg/orchestration/wiring/kubeingress/plugin.go
+++ pkg/orchestration/wiring/kubeingress/plugin.go
@@ -13,6 +13,7 @@ import (
 	"github.com/atlassian/voyager/pkg/orchestration/wiring/k8scompute/api"
 	"github.com/atlassian/voyager/pkg/orchestration/wiring/wiringplugin"
 	"github.com/atlassian/voyager/pkg/orchestration/wiring/wiringutil"
+	"github.com/atlassian/voyager/pkg/orchestration/wiring/wiringutil/knownshapes"
 	"github.com/pkg/errors"
 	apps_v1 "k8s.io/api/apps/v1"
 	core_v1 "k8s.io/api/core/v1"
@@ -70,6 +71,11 @@ func WireUp(resource *orch_v1.StateResource, context *wiringplugin.WiringContext
 	}
 
 	result := &wiringplugin.WiringResult{
+		Contract: wiringplugin.ResourceContract{
+			Shapes: []wiringplugin.Shape{
+				knownshapes.NewIngressEndpoint(ingressResource.SmithResource.Name),
+			},
+		},
 		Resources: []wiringplugin.WiredSmithResource{serviceResource, ingressResource},
 	}
 
@@ -189,7 +195,7 @@ func buildIngressResourceFromSpec(serviceName smith_v1.ResourceName, resourceNam
 				},
 			},
 		},
-		Exposed: true,
+		Exposed: false,
 	}
 
 	return ingressResource, nil
