a680e0eeb53cdc8ff5aba971851bb61ee60fb26e
Yiyang Chen <ychen@atlassian.com>
2019-01-15 11:05:05 +1100

    VYGR-349 added new secretplugin

---
+++ pkg/execution/plugins/generic/secretplugin/secret_plugin.go
@@ -0,0 +1,56 @@
+package secretplugin
+
+import (
+	smith_plugin "github.com/atlassian/smith/pkg/plugin"
+	"github.com/pkg/errors"
+	core_v1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+const (
+	PluginName = "secret"
+)
+
+type Plugin struct{}
+
+func New() (smith_plugin.Plugin, error) {
+	return &Plugin{}, nil
+}
+
+func (p *Plugin) Describe() *smith_plugin.Description {
+	return &smith_plugin.Description{
+		Name: PluginName,
+		GVK:  core_v1.SchemeGroupVersion.WithKind("Secret"),
+		SpecSchema: []byte(`
+			{
+				"type": "object",
+				"properties": {
+					"data": {
+						"type": "object"
+					}
+				},
+				"additionalProperties": false,
+				"required": ["data"]
+			}
+		`),
+	}
+}
+
+func (p *Plugin) Process(rawSpec map[string]interface{}, context *smith_plugin.Context) (*smith_plugin.ProcessResult, error) {
+	spec := Spec{}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(rawSpec, &spec)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to convert into typed spec")
+	}
+
+	return &smith_plugin.ProcessResult{
+		Object: &core_v1.Secret{
+			TypeMeta: meta_v1.TypeMeta{
+				APIVersion: core_v1.SchemeGroupVersion.String(),
+				Kind:       "Secret",
+			},
+			Data: spec.Data,
+		},
+	}, nil
+}
---
+++ pkg/execution/plugins/generic/secretplugin/types.go
@@ -0,0 +1,5 @@
+package secretplugin
+
+type Spec struct {
+	Data map[string][]byte `json:"data"`
+}
