2e47a0dda508e2e2d1935d178352ad6d81796b0c
Nail Islamov <nislamov@atlassian.com>
2019-01-22 13:45:25 +1100

    Make OSB wiring easier to read

---
+++ pkg/orchestration/wiring/wiringutil/osb/service_instance_wiring_test.go
@@ -0,0 +1,22 @@
+package osb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func TestInstanceId(t *testing.T) {
+	t.Parallel()
+
+	expected := "washere"
+	actual, err := instanceID(
+		&runtime.RawExtension{
+			Raw: []byte(`{"instanceId":"` + expected + `"}`),
+		},
+	)
+	require.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
