ab462bc786a5a7db8cc1d4de2daba089f14cfbf3
Elliot Pahl <epahl@atlassian.com>
2019-01-23 13:48:55 +1100

    MICROS-6858 Add default env vars to orchestration

--- pkg/orchestration/wiring/k8scompute/plugin.go
+++ pkg/orchestration/wiring/k8scompute/plugin.go
@@ -119,6 +119,7 @@ func WireUp(resource *orch_v1.StateResource, context *wiringplugin.WiringContext
 		return nil, false, err
 	}
 	// Prepare environment variables
+	var envDefault []core_v1.EnvVar
 	var envFrom []core_v1.EnvFromSource
 	var smithResources []smith_v1.Resource
 	var bindingResult []compute.BindingResult
@@ -226,13 +227,13 @@ func WireUp(resource *orch_v1.StateResource, context *wiringplugin.WiringContext
 	}
 	references = append(references, serviceAccountNameRef)
 
-	// default env vars for containers
-	var envDefault []core_v1.EnvVar
-
 	// ASAP public key servers
-	// we want every container to know where to get the public keys
-	// regardless if they're using ASAP or not
+	// - All containers should know where to get the public keys
+	//   regardless if they're using ASAP or not
 	envDefault = append(envDefault, asapkey.GetPublicKeyRepoEnvVars(context.StateContext.Location)...)
+
+	// Add Micros provided defaults
+	envDefault = append(envDefault, buildDefaultEnvVars(resource.Name, context.StateContext.Location)...)
 
 	// always bind to the common secret, it's OK if it doesn't exist
 	trueVar := true
@@ -441,6 +442,23 @@ func buildContainers(spec *Spec, envDefault []core_v1.EnvVar, envFrom []core_v1.
 	}
 
 	return containers
+}
+
+func buildDefaultEnvVars(name voyager.ResourceName, location voyager.Location) []core_v1.EnvVar {
+	return []core_v1.EnvVar{
+		{
+			Name:  "MICROS_AWS_REGION",
+			Value: string(location.Region),
+		},
+		{
+			Name:  "MICROS_ENVTYPE",
+			Value: string(location.EnvType),
+		},
+		{
+			Name:  "MICROS_SERVICE",
+			Value: string(name),
+		},
+	}
 }
 
 func buildAffinity(labelMap map[string]string) *core_v1.Affinity {
