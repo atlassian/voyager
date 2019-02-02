ab901abd578a43d32cdbae6a605daa306b1926e5
Fraser Cobb <fcobb@atlassian.com>
2019-02-02 15:52:09 +1100

    VYGR-391: Add more GetServiceAttribute tests

--- pkg/servicecentral/client_test.go
+++ pkg/servicecentral/client_test.go
@@ -289,11 +289,45 @@ func TestGetServiceWithOpsGenieAttribute(t *testing.T) {
 	defer serviceCentralServerMock.Close()
 	// when
 	serviceCentralClient := testServiceCentralClient(t, serviceCentralServerMock.URL, pkitest.MockASAPClientConfig(t))
-	_, err := serviceCentralClient.GetService(context.Background(), optionalUser, string(testServiceName))
+	service, err := serviceCentralClient.GetService(context.Background(), optionalUser, string(testServiceName))
 
 	// then
 	require.NoError(t, err)
 	require.Equal(t, 2, handler.RequestSnapshots.Calls())
+
+	require.Equal(t, 1, len(service.Attributes))
+	require.Equal(t, "Platform SRE", service.Attributes[0].Team)
+}
+
+func TestGetServiceWithEmptyOpsGenieAttribute(t *testing.T) {
+	t.Parallel()
+	// given
+	handler := MockHandler(Match(
+		Method(http.MethodGet),
+		Path(fmt.Sprintf("%s/%s", v1ServicesPath, testServiceName)),
+	).Respond(
+		Status(http.StatusOK),
+		JSONFromFile(t, "get_service.rsp.json"),
+	),
+		Match(
+			Method(http.MethodGet),
+			Path(fmt.Sprintf("%s/%s/attributes", v2ServicesPath, testServiceName)),
+		).Respond(
+			Status(http.StatusOK),
+			JSONFromFile(t, "get_service_attributes_empty_team_string.rsp.json"),
+		))
+	serviceCentralServerMock := httptest.NewServer(handler)
+	defer serviceCentralServerMock.Close()
+	// when
+	serviceCentralClient := testServiceCentralClient(t, serviceCentralServerMock.URL, pkitest.MockASAPClientConfig(t))
+	service, err := serviceCentralClient.GetService(context.Background(), optionalUser, string(testServiceName))
+
+	// then
+	require.NoError(t, err)
+	require.Equal(t, 2, handler.RequestSnapshots.Calls())
+
+	require.Equal(t, 1, len(service.Attributes))
+	require.Equal(t, "", service.Attributes[0].Team)
 }
 
 func TestGetServiceWithoutOpsGenieAttribute(t *testing.T) {
@@ -317,11 +351,12 @@ func TestGetServiceWithoutOpsGenieAttribute(t *testing.T) {
 	defer serviceCentralServerMock.Close()
 	// when
 	serviceCentralClient := testServiceCentralClient(t, serviceCentralServerMock.URL, pkitest.MockASAPClientConfig(t))
-	_, err := serviceCentralClient.GetService(context.Background(), optionalUser, string(testServiceName))
+	service, err := serviceCentralClient.GetService(context.Background(), optionalUser, string(testServiceName))
 
 	// then
 	require.NoError(t, err)
 	require.Equal(t, 2, handler.RequestSnapshots.Calls())
+	require.Equal(t, 0, len(service.Attributes))
 }
 
 func TestGetServiceWithFailedAttributesCall(t *testing.T) {
@@ -344,11 +379,12 @@ func TestGetServiceWithFailedAttributesCall(t *testing.T) {
 	defer serviceCentralServerMock.Close()
 	// when
 	serviceCentralClient := testServiceCentralClient(t, serviceCentralServerMock.URL, pkitest.MockASAPClientConfig(t))
-	_, err := serviceCentralClient.GetService(context.Background(), optionalUser, string(testServiceName))
+	service, err := serviceCentralClient.GetService(context.Background(), optionalUser, string(testServiceName))
 
 	// then
 	require.NoError(t, err) // Expect no error as OpsGenie team is optional
 	require.Equal(t, 2, handler.RequestSnapshots.Calls())
+	require.Equal(t, 0, len(service.Attributes))
 }
 
 func testServiceCentralClient(t *testing.T, serviceCentralServerMockAddress string, asap pkiutil.ASAP) *Client {
