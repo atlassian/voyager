732c738a80d6edc39281e468a96cb5cae9b4b7db
scottgreenup <me@scottgreenup.com>
2019-02-12 16:33:33 +1100

    Logging; reduce nesting; review nits

--- pkg/reporterreporter/report.go
+++ pkg/reporterreporter/report.go
@@ -3,6 +3,7 @@ package reporterreporter
 import (
 	"context"
 	"net/http"
+	"net/url"
 	"time"
 
 	"bitbucket.org/atlassianlabs/restclient"
@@ -19,16 +20,16 @@ import (
 const (
 	retryAttemptsPerReport = 5
 	retryDelay             = time.Second * 1
+	slurperPath            = "/slurp"
 )
 
-type Report struct {
-	Cluster          string
-	HTTPClient       *http.Client
-	ReporterClient   client.Interface
-	KubernetesClient kubernetes.Interface
-	Logger           *zap.Logger
-
-	mutator *restclient.RequestMutator
+type report struct {
+	clusterID        string
+	httpClient       *http.Client
+	kubernetesClient kubernetes.Interface
+	logger           *zap.Logger
+	mutator          *restclient.RequestMutator
+	reporterClient   client.Interface
 }
 
 type RequestData struct {
@@ -39,17 +40,21 @@ type RequestData struct {
 }
 
 func NewReport(slurperURI string, cluster string, reporterClient client.Interface, kubeClient kubernetes.Interface, logger *zap.Logger) ctrl.Server {
-	return &Report{
-		Cluster:          cluster,
-		HTTPClient:       util.HTTPClient(),
-		ReporterClient:   reporterClient,
-		KubernetesClient: kubeClient,
-		Logger:           logger,
+	logger.Info("ReporterReporter configured",
+		zap.String("cluster_id", cluster),
+		zap.String("slurper_uri", slurperURI),
+		zap.String("slurper_path", slurperPath))
 
+	return &report{
+		clusterID:        cluster,
+		httpClient:       util.HTTPClient(),
+		kubernetesClient: kubeClient,
+		logger:           logger,
 		mutator: restclient.NewRequestMutator(
 			restclient.BaseURL(slurperURI),
-			restclient.JoinPath("/slurp"),
+			restclient.JoinPath(slurperPath),
 			restclient.Method(http.MethodPost)),
+		reporterClient: reporterClient,
 	}
 }
 
@@ -66,68 +71,103 @@ func isRetriableError(statusCode int) bool {
 	return statusCode >= 500 && statusCode <= 599
 }
 
-func (r *Report) sendData(requestData RequestData) (retriable bool, err error) {
+// sendData will send the requestData containing the report to slurper
+func (r *report) sendData(requestData RequestData) (retriable bool, err error) {
 	req, err := r.mutator.NewRequest(restclient.BodyFromJSON(requestData))
 	if err != nil {
-		return false, errors.Wrap(err, "could not craft a HTTP request")
+		return false, errors.Wrap(err, "unable to craft a HTTP request for slurper")
 	}
 
-	resp, err := r.HTTPClient.Do(req)
+	resp, err := r.httpClient.Do(req)
 	if err != nil {
-		return true, errors.Wrap(err, "failed to perform HTTP request to slurper")
-	}
+		if urlError, ok := err.(*url.Error); ok {
+			if urlError.Timeout() || urlError.Temporary() {
+				return true, errors.Wrap(err, "transient error occurred during request to slurper")
+			}
+		}
 
+		return false, errors.Wrap(err, "error occured during request to slurper")
+
+	}
+	defer resp.Body.Close()
+
+	// We don't need to check if resp is nil; it won't be if err is nil
 	if resp.StatusCode == http.StatusOK {
 		return false, nil
 	}
 
-	if isRetriableError(resp.StatusCode) {
-		return true, errors.Errorf("POSTing data to slurper failed with retriable status code %d", resp.StatusCode)
-	}
-
-	return false, errors.Errorf("POSTing data to slurper failed with status code %d", resp.StatusCode)
+	return isRetriableError(resp.StatusCode), errors.Errorf("sending data to slurper failed")
 }
 
-func (r *Report) Run(ctx context.Context) error {
-	namespaces, err := r.KubernetesClient.CoreV1().Namespaces().List(meta_v1.ListOptions{})
+func (r *report) sendNamespaceReportToSlurper(namespaceName string) (notRetriableError error) {
+	reports, err := r.reporterClient.ReporterV1().Reports(namespaceName).List(meta_v1.ListOptions{})
+	if err != nil {
+		r.logger.Error("Could not list reports in namespace", zap.String("namespace", namespaceName), zap.Error(err))
+		return nil
+	}
+
+	if len(reports.Items) == 0 {
+		r.logger.Info("Report for namespace is empty", zap.String("namespace", namespaceName))
+		return nil
+	}
+
+	now := time.Now().Unix()
+
+	countReportsSent := 0
+	countReportsAttempted := 0
+
+	for _, report := range reports.Items {
+		requestData := RequestData{
+			Cluster:   r.clusterID,
+			Service:   namespaceName,
+			Timestamp: now,
+			Data:      report.Report,
+		}
+		countReportsAttempted++
+
+		for i := 1; i <= retryAttemptsPerReport; i++ {
+			retriable, err := r.sendData(requestData)
+			if err == nil {
+				countReportsSent++
+				break
+			}
+
+			if !retriable {
+				// If the request can not be retried, then we shouldn't try continue trying requests
+				r.logger.Info("Sent namespace reports to slurper",
+					zap.String("namespace_name", namespaceName),
+					zap.Int("reports_sent", countReportsSent),
+					zap.Int("reports_attempted", countReportsAttempted),
+					zap.Int("reports_size", len(reports.Items)))
+				return err
+			}
+
+			r.logger.Warn("Failed to send report to slurper",
+				zap.Int("attempt", i),
+				zap.Int("max_attempts", retryAttemptsPerReport),
+				zap.Error(err))
+			time.Sleep(retryDelay)
+		}
+	}
+
+	r.logger.Info("Sent namespace reports to slurper",
+		zap.String("namespace_name", namespaceName),
+		zap.Int("reports_sent", countReportsSent),
+		zap.Int("reports_attempted", countReportsAttempted),
+		zap.Int("reports_size", len(reports.Items)))
+
+	return nil
+}
+
+func (r *report) Run(_ context.Context) error {
+	namespaces, err := r.kubernetesClient.CoreV1().Namespaces().List(meta_v1.ListOptions{})
 	if err != nil {
 		return errors.Wrap(err, "failed to list namespaces")
 	}
 
 	for _, namespace := range namespaces.Items {
-		reports, err := r.ReporterClient.ReporterV1().Reports(namespace.Name).List(meta_v1.ListOptions{})
-		if err != nil {
-			r.Logger.Error("Could not list reports in namespace", zap.String("namespace", namespace.Name), zap.Error(err))
-			continue
-		}
-
-		if len(reports.Items) == 0 {
-			r.Logger.Info("Report for namespace is empty", zap.String("namespace", namespace.Name))
-			continue
-		}
-
-		for _, report := range reports.Items {
-			requestData := RequestData{
-				Cluster:   r.Cluster,
-				Service:   namespace.Name,
-				Timestamp: time.Now().Unix(),
-				Data:      report.Report,
-			}
-
-			for i := 1; i <= retryAttemptsPerReport; i++ {
-				if retriable, err := r.sendData(requestData); err != nil {
-					if !retriable {
-						// If the request is not retriable, then we shouldn't try the other requests either
-						return err
-					} else {
-						r.Logger.Warn("Failed to send request data to slurper",
-							zap.Int("attempt", i),
-							zap.Int("max_attempts", retryAttemptsPerReport),
-							zap.Error(err))
-						time.Sleep(retryDelay)
-					}
-				}
-			}
+		if err := r.sendNamespaceReportToSlurper(namespace.Name); err != nil {
+			return errors.Wrap(err, "non-retriable error encountered while talking to slurper, stopping all reports")
 		}
 	}
 
